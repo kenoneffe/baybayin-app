1b49d2df5167e359c8ceb2dbfc017f09e1ea27e0
jtsbellezas <33455812+jtsbellezas@users.noreply.github.com>
2018-12-13 00:19:23 +0800

    added: 1. dots indicator

--- app/src/main/java/com/baybayinapp/thesis/baybayin_app/History.java
+++ app/src/main/java/com/baybayinapp/thesis/baybayin_app/History.java
@@ -3,11 +3,18 @@ package com.baybayinapp.thesis.baybayin_app;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class History extends AppCompatActivity {
 
     private ViewPager mSlideViewPager;
+    private LinearLayout mDotLayout;
+
+    private TextView[] mDots;
+
     private SlideAdapter sliderAdapter;
 
     @Override
@@ -16,8 +23,25 @@ public class History extends AppCompatActivity {
         setContentView(R.layout.activity_history);
 
         mSlideViewPager = (ViewPager) findViewById(R.id.slideViewPager);
+        mDotLayout = (LinearLayout) findViewById(R.id.dotsLayout);
 
         sliderAdapter = new SlideAdapter(this);
         mSlideViewPager.setAdapter(sliderAdapter);
+
+        addDotsIndicator();
+    }
+
+    public void addDotsIndicator(){
+
+        mDots = new TextView[3];
+
+        for(int i = 0; i < mDots.length; i++){
+            mDots[i] = new TextView(this);
+            mDots[i].setText(Html.fromHtml("8226;"));
+            mDots[i].setTextSize(35);
+            mDots[i].setTextColor(getResources().getColor(R.color.colorTransparentWhite));
+
+            mDotLayout.addView(mDots[i]);
+        }
     }
 }
